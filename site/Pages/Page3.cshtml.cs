f49c9e26b5abace5bec09cf5486f33a9d827ba57
boris <boris@DESKTOP-R9HMLU6>
2024-02-23 20:40:55 +0300

    Добавьте файлы проекта.

---
+++ site/Pages/Page3.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace site.Pages
+{
+    public class Page3 : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
