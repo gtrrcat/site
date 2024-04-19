4df2ee504894344da31c1da3d37bbbf968c5f0b2
Павел Иштиряков <pavlik4a@gmail.com>
2024-04-20 00:38:06 +0300

    рррр

--- site/Data/Models/User.cs
+++ site/Data/Models/User.cs
@@ -4,13 +4,11 @@
     {
         public int Id { get; set; } = 0;
         public string Name { get; set; }
-
         public int Age { get; set; }
+
         public string Surname { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
-            
-        
     }
 }
