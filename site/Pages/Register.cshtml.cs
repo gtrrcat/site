91a371af059ecee20ab27136865361cf4066b631
gtrrcat <borisov.nik2004@yandex.ru>
2024-04-19 22:22:38 +0300

    Добавил модель

--- site/Pages/Register.cshtml.cs
+++ site/Pages/Register.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-
+using site.Data.Models;
+using System.Web;
 namespace site.Pages
 {
     public class Index1Model : PageModel
     {
         public void OnGet()
         {
+        }
+        public void AddResponse(User user) 
+        {
+            Console.WriteLine(user.Name);
         }
     }
 }
